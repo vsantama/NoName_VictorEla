f3635491337969fffb7653b341e41bc1641ba683
Elaphernelia <elakshep@ucm.es>
2020-03-04 14:57:57 +0100

    Intento de poner al perrete

--- src/phasertemplate-master/game.js
+++ src/phasertemplate-master/game.js
@@ -3,11 +3,14 @@ export default class Game extends Phaser.Scene {
     super({ key: 'main' });
   }
   preload() {  
+    this.load.image('front_shiba', 'src/TubuRun/Assets/shiba_front.png')
   }//images, sprites, etc
 
   create() {//"poner las cosas"
+    this.add.image(200, 100, 'front_shiba');
   }
 
   update(time, delta) {    //interactions
+    
   }
 }
